9d882791addbf981ea8cfa3dbf47c02844b5ad89
chrisrw <crwargo0783@gmail.com>
2020-03-03 20:29:15 -0500

    initial setup complete

---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE what_cheer;
+CREATE USER what_cheer_user WITH PASSWORD 'what_cheer';
+GRANT ALL PRIVILEGES ON DATABASE what_cheer TO what_cheer_user
---
+++ what_cheer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WhatCheerConfig(AppConfig):
+    name = 'what_cheer'
