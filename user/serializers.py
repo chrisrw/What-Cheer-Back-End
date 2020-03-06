236c4fd9f136ed921ef61375025fc75149f1bda0
aafmisra <aafmisra@gmail.com>
2020-03-06 18:39:11 -0500

    tweaked user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -31,4 +31,4 @@ class UserSerializerWithToken(serializers.ModelSerializer):
     class Meta:
         model = User
 
-        fields = ('token', 'username', 'password', )
+        fields = ('token', 'username', 'email', 'password', )
