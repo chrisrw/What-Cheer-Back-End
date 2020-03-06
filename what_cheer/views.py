fdd35f781d8a9824f03a0ffcb92170f286ab501d
chrisrw <crwargo0783@gmail.com>
2020-03-06 12:55:00 -0500

    added permissions for what_cheer views

--- what_cheer/views.py
+++ what_cheer/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions, status
+from rest_framework import generics, permissions
 from .serializers import EntrySerializer, PromptSerializer, UserSerializer, UserSerializerWithToken
 from .models import Entry, Prompt
 from rest_framework.response import Response
@@ -8,11 +8,14 @@ from django.contrib.auth.models import User
 class EntryList(generics.ListCreateAPIView):
     queryset = Entry.objects.all()
     serializer_class = EntrySerializer
+    permission_classes = [permissions.IsAuthenticated]
 class EntryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Entry.objects.all()
     serializer_class = EntrySerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class PromptDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Prompt.objects.all()
     serializer_class = PromptSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
